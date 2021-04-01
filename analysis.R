b0067247dadb535accd4ad7ba1ae9e39cd22719c
Meredith Duffy <meredithduffy@Merediths-MacBook-Pro-2.local>
2021-04-01 17:26:05 -0400

    2 charts down, 1 to go

--- analysis.R
+++ analysis.R
@@ -1,12 +1,32 @@
 complaints <- readRDS("complaints.rds")
 
-### states with highest number of complaints
 
+
+### states and complaints
 complaints %>% count(state)
 
 state <- complaints %>% 
-  count(state) %>% arrange(desc(n))
+  count(state) %>% arrange(desc(states)) %>% 
 
 glimpse(state)
 View(state)
+
+
+
+### companies and complaints
+complaints %>% count(company)
+
+company <- complaints %>% 
+  count(company) %>% arrange(desc(n)) 
+
+glimpse(company)
+View(company)
+
+
+
+### companies that gave monetary relief
+complaints %>% 
+  filter(company_response_to_consumer == "Closed with monetary relief") %>% 
+  select(company) %>% 
+  head(10)
 
